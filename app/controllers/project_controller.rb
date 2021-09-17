604028ae6a004cd165025b0c020ee3744817b76c
EmilioAsteben <butt-head333@mail.ru>
2021-09-17 06:31:34 +0300

    API

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,5 +1,46 @@
 class ProjectController < ApplicationController
+
   def index
-    render json: {message:"ok"}, status: 200
+    @projects = Project.all
+    render json: @projects, include: 'todos'
   end
+
+  def todos
+
+    if params[:project_id] && params[:project_name].blank?
+    
+      @project = Project.find(params[:project_id])
+
+      @todo = @project.todos.create(text: params[:task_text], isCompleted: false)
+      
+
+    elsif params[:project_id].blank? && params[:project_name]
+
+      @project = Project.new(title: params[:project_name])
+      
+      if @project.save
+
+      @project.todos.create(text: params[:task_text], isCompleted: false)
+
+      end
+    
+  end
+    
+end
+
+
+
+def patch 
+
+  @project = Project.find(params[:projectid])
+  @isCompleted = @project.todos.find(params[:todoid])[:isCompleted]
+
+  
+  if @todo = @project.todos.find(params[:todoid])
+  @todo.update(isCompleted: !@isCompleted)
+
+   end
+
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # resources :project, :path => "projects"
   get "/projects", to: "project#index"
+  post "/todos", to: "project#todos"
+  patch "/projects/:projectid/todo/:todoid", to: "project#patch"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
