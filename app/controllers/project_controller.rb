26ddcb22535186c56055cf786757d15da969b3df
Emilio <asteben@bk.ru>
2021-09-29 23:34:16 +0300

    validation

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -12,21 +12,30 @@ class ProjectController < ApplicationController
 
     if params[:project_id] && params[:project_name].blank? && params[:task_text]
     
-      @project = Project.find(params[:project_id])
 
-      render json: @todo = @project.todos.create(text: params[:task_text], isCompleted: false)
+      @data =  Project.find(params[:project_id]).todos.where(text: params[:task_text], isCompleted: false ).first_or_create do |obj|
+        @just_created = true
+      end
+      if @just_created
+        render json: @data
+      end
       
 
     elsif params[:project_id].blank? && params[:project_name] && params[:task_text]
 
+     
 
-       @project = Project.new(title: params[:project_name])
+       @project = Project.where(title: params[:project_name]).first_or_create do |obj|
+        @just_created = true
+       end
+       
+       
       
-      if @project.save
+      if @project.save  &&  @just_created === true
 
         @data = PostData.new()
 
-        @data.todo = @project.todos.create(text: params[:task_text], isCompleted: false)
+        @data.todo = @project.todos.where(text: params[:task_text], isCompleted: false).first_or_create
         @data.category_title = Project.find(@data.todo.project_id).title
       
         render json: @data
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
-    has_many :todos , -> { order(text: :desc) },  dependent: :destroy
+    has_many :todos , -> { order(id: :asc) },  dependent: :destroy
+
+    validates :title, presence: true, length: { minimum: 4 }
 end
